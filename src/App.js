031566504c2bd0703e318c86bcb62a5e84d6e8e2
Irene Buranayotkul <irene.b@solarcity.co.nz>
2020-12-18 09:06:50 +1300

    Added .env to ignore

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import dotenv from 'dotenv';
 import React from 'react';
 import {Container, Row, Col} from 'reactstrap';
 import Header from './components/Header';
@@ -7,7 +6,6 @@ import Calendar from './components/Calendar';
 import ToDoSummary from "./components/ToDoSummary";
 import SideBar from "./components/SideBar";
 function App() {
-    console.log(process.env.REACT_OPEN_API_KEY);
   return (
     <div className="App">
       <Header/>
